335414d8824670e352cad42d523d9a7a6b39d65c
Priyanshu Singh <priyanshu.singh@joshtechnologygroup.com>
2026-02-04 15:45:22 +0530

    [PS_AA_01] : change the base pr template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './shared/components/404-page/not-found/not-found.component';
 
-export const routes: Routes = [
-
-    { path: '**', component: NotFoundComponent }
-];
+export const routes: Routes = [{ path: '**', component: NotFoundComponent }];
