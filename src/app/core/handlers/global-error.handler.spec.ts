b0b5b4f89f9053efcbbd2a74963d592c8fbaa3d3
Priyanshu Singh <priyanshu.singh@joshtechnologygroup.com>
2026-02-04 15:38:59 +0530

    [PS_AA_01] : Added the global error handler and the 404 page

---
+++ src/app/core/handlers/global-error.handler.spec.ts
@@ -0,0 +1,7 @@
+import { GlobalError } from './global-error.handler';
+
+describe('GlobalError', () => {
+  it('should create an instance', () => {
+    expect(new GlobalError()).toBeTruthy();
+  });
+});
