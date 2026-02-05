cfe674e605aab192ae23c740edbaea3aaab819c5
Priyanshu Singh <priyanshu.singh@joshtechnologygroup.com>
2026-02-05 14:54:36 +0530

    config the global error handler

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, ErrorHandler } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { GlobalError } from './core/handlers/global-error.handler';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    { provide: ErrorHandler, useClass: GlobalError },
+  ],
 };
