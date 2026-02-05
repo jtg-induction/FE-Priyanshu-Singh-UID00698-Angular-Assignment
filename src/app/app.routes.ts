4a8a8798c4d1a5344c45c25adc197d1421a44b09
Priyanshu Singh <priyanshu.singh@joshtechnologygroup.com>
2026-02-05 14:43:01 +0530

    stop tracking .vscode

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+
 import { NotFoundComponent } from './shared/components/404-page/not-found/not-found.component';
 
 export const routes: Routes = [{ path: '**', component: NotFoundComponent }];
